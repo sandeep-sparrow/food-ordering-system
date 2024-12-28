a52f5c9f55bc2f03d253836e67589e3ad68a2d3f
Sandeep prajapati <61901052+sandeep-sparrow@users.noreply.github.com>
2024-12-29 02:35:31 +0530

    implementing order-dataaccess

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntityId.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemEntityId implements Serializable {
+
+    private Long id;
+    private OrderEntity order;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntityId that = (OrderItemEntityId) o;
+        return Objects.equals(id, that.id) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+}
